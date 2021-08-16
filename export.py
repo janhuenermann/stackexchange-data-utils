9a4ad457244440865c670b5c26b0dab554c25f72
Jan Hünermann <jan@huenermann.de>
2021-08-16 17:50:23 +0200

    Export

--- export.py
+++ export.py
@@ -3,6 +3,7 @@ from stackexchange.generator import generate
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('db', type=str, help='Path to database in which to store dataset')
+parser.add_argument('out_dir', type=str, help='Path to output directory')
 args = parser.parse_args()
 
-generate(args.db)
+generate(args.db, args.out_dir)
--- stackexchange/generator.py
+++ stackexchange/generator.py
@@ -4,7 +4,7 @@ import click
 from tqdm import tqdm
 
 
-def generate(db_path):
+def generate(db_path, out_dir, max_chunk_size=100_000_000):
     try:
         db = sqlite3.connect(db_path)
     except Error as e:
@@ -16,15 +16,37 @@ def generate(db_path):
 
     cur = db.cursor()
     cur.execute("""
-
         SELECT question.body, answer.body FROM posts answer
         INNER JOIN posts AS question ON question.post_id = answer.parent_id AND question.site_id = answer.site_id
-        WHERE answer.post_type = 2;
+        WHERE answer.post_type = 2;""")
 
-        """)
+    chunk_format = "chunk_{:04d}.txt"
+    chunk_index = -1
+    fd = None
+
+    def next_chunk():
+        nonlocal chunk_index
+        nonlocal fd
+        chunk_index += 1
+        chunk_path = os.path.join(out_dir, chunk_format.format(chunk_index))
+        if fd is not None:
+            fd.close()
+        fd = open(chunk_path, "w")
+
+    os.makedirs(out_dir)
+    next_chunk()
+    endian = "big"
 
     for item in tqdm(cur, total=total):
-        pass
+        question, answer = item
+        qlen = len(question)
+        rlen = qlen + len(answer)
+        # [row length], [question length], [question string], [answer string]
+        fd.write(rlen.to_bytes(4, endian))
+        fd.write(qlen.to_bytes(4, endian))
+        fd.write(question + answer)
+        if fd.tell() > max_chunk_size:
+            next_chunk()
 
     cur.close()
 
